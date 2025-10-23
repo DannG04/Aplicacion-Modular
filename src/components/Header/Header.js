14cdc7725e18880977a423bf6294d90a3894d807
DannG04 <2022060488_li@unsis.edu.mx>
2025-10-23 11:10:02 -0600

    Agrega logo y reacomoda items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // <-- Importar Link
 import './Header.css';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import logo from './logo.png';
 
 const Header = () => {
   return (
     <header className="app-header">
       <div className="logo-nav">
-        <h1 className="logo">Mi App</h1>
-        <nav>
-          {/* Usamos <Link> en lugar de <a href=""> */}
-          <Link to="/">Inicio</Link>
-          <Link to="/tareas">Tareas</Link>
-          <Link to="/directorio">Directorio</Link>
-        </nav>
+
+        <img src={logo} alt="Logo" className="logo" />
+        <div >
+          <nav>
+            {/* Usamos <Link> en lugar de <a href=""> */}
+            <Link to="/" >Inicio</Link>
+            <Link to="/tareas">Tareas</Link>
+            <Link to="/directorio">Directorio</Link>
+          </nav>
+        </div>
       </div>
       <ThemeSwitcher />
     </header>
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Página no encontrada</h2>
+      <p>Lo sentimos, la página que buscas no existe.</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
